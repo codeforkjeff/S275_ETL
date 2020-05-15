70ea52664f1c5306ebed3791c61bbd7999b1bc4f
Jeff Chiu <jchiu@ccedresults.org>
2020-05-14 19:39:09 -0700

    missing extension

--- S275_settings_sample.py
+++ S275_settings_sample.py
@@ -22,7 +22,7 @@ source_files = [
 	,(os.path.join(input_dir, "2015-2016_Final_S-275_Personnel_Database.accdb"), "FINAL")
 	,(os.path.join(input_dir, "2016-2017_Final_S-275_Personnel_Database.accdb"), "FINAL")
 	,(os.path.join(input_dir, "2017-2018FinalS-275PersonnelDatabase.accdb"), "FINAL")
-	,(os.path.join(input_dir, "2018-2019_Final_S-275_Personnel_Database"), "FINAL")
+	,(os.path.join(input_dir, "2018-2019_Final_S-275_Personnel_Database.accdb"), "FINAL")
 ]
 
 # Dim_School_Fields table gets loaded from this only if file exists
