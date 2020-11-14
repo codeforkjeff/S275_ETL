8653454d12b3f4699129ee4148dc8f260494f9f0
Jeff Chiu <jchiu@ccedresults.org>
2020-11-13 19:09:53 -0800

    implement index macros for SQL Server

--- macros/indexes.sql
+++ macros/indexes.sql
@@ -4,6 +4,15 @@
 	{%- if adapter.config.credentials.type == 'sqlite' -%}
 		CREATE {% if unique %}UNIQUE {% endif %} INDEX {{ this.schema }}.idx_{{ prefix|string }}_{{ this.table }} ON {{ this.table }}
 		({{ '"' + columns|join('", "') + '"' }})
+	{% elif adapter.config.credentials.type == 'sqlserver' %}
+		{#
+		dbt-sqlserver auto-creates column store indexes, so make all create_index() calls no-ops.
+
+		--CREATE {% if unique %}UNIQUE {% endif %} INDEX idx_{{ prefix|string }}_{{ this.table }} ON {{ this }}
+		--({{ '"' + columns|join('", "') + '"' }})
+		#}
+
+		SELECT 1
 	{% else %}
 		{{ exceptions.raise_not_implemented("create_index not implemented for this database type") }}
 	{% endif %}
@@ -14,6 +23,8 @@
 
 	{%- if adapter.config.credentials.type == 'sqlite' -%}
 		DROP INDEX IF EXISTS {{ this.schema }}.idx_{{ prefix|string }}_{{ this.table }}
+	{% elif adapter.config.credentials.type == 'sqlserver' %}
+		DROP INDEX IF EXISTS idx_{{ prefix|string }}_{{ this.table }} ON {{this}}
 	{% else %}
 		{{ exceptions.raise_not_implemented("drop_index not implemented for this database type") }}
 	{% endif %}
