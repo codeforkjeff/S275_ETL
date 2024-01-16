05214871ac6658eda8ba3826b5bc07e0f36ee8db
Jeff Chiu <jeffchiu@uw.edu>
2024-01-16 10:44:42 -0800

    add dbt test

--- elt.sh
+++ elt.sh
@@ -52,6 +52,8 @@ python -c "import S275; S275.create_ext_duty_list()"
 # Re-run the specific transforms that depend on the above re-created tables.
 dbt run -m source:ext+
 
+dbt test
+
 echo "********************************************************"
 echo "**** ELT is finished, starting documentation server ****"
 echo "********************************************************"
